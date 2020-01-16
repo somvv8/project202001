0d1d156e3ec4011c11b5115336d29d35a451c1df
bit <arri88@naver.com>
2020-01-16 13:11:29 +0900

    som FrontController ok

--- src/com/bitcamp/comm/FrontController.java
+++ src/com/bitcamp/comm/FrontController.java
@@ -1,16 +1,26 @@
 package com.bitcamp.comm;
 
+import java.io.FileReader;
 import java.io.IOException;
+import java.util.Enumeration;
+import java.util.Hashtable;
+import java.util.Properties;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+
 /**
  * Servlet implementation class FrontController
  */
-@WebServlet("*.do")
+@WebServlet(urlPatterns= {"*.do"}
+,initParams= {@WebInitParam(name="init",value="/WEB-INF/prop.properties")})
 public class FrontController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
@@ -21,13 +31,43 @@ public class FrontController extends HttpServlet {
         super();
         // TODO Auto-generated constructor stub
     }
+    
+    private Hashtable<String,Action> ht=new Hashtable<>();
+    
+
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		// TODO Auto-generated method stub
+		String initial=config.getInitParameter("init");
+		Properties prop=new Properties();
+		try 
+		{
+			String path=
+						config.getServletContext().getRealPath(initial);
+			prop.load(new FileReader(path));
+			Enumeration enu=prop.keys();
+			while(enu.hasMoreElements())
+			{
+				String key=(String)enu.nextElement();
+				String value=prop.getProperty(key);
+				Class c=Class.forName(value);
+				Action obj=(Action)c.newInstance();
+				ht.put(key, obj);
+			}
+			
+		}catch(Exception e) {
+			System.out.println(e);
+		}
+		
+		
+	}
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		doReq(request, response);
 	}
 
 	/**
@@ -35,7 +75,27 @@ public class FrontController extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
+		doReq(request, response);
+	}
+	
+	private void doReq(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+		String path=request.getServletPath();
+		Action act=null;
+		act=ht.get(path);
+		System.out.println("object: "+act);
+		
+		ActionForward forward=act.execute(request, response);
+		if(forward.isForward()) 
+		{
+			RequestDispatcher disp
+				=request.getRequestDispatcher(forward.getUrl());
+			disp.forward(request, response);
+		}else
+		{
+			response.sendRedirect(forward.getUrl());
+		}
+		
+		
 	}
 
 }
